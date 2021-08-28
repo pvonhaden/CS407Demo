13ca0a988d71f88b99c16b129c30ffbb389013cf
Mohit Loganathan <lmohit95@gmail.com>
2021-08-28 10:41:33 -0500

    CS 407 Demo File

---
+++ Operations.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Operations {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int firstNumber = in.nextInt();
+		
+		System.out.println("Enter the second number");
+		int secondNumber = in.nextInt();
+		
+		System.out.println("Sum = " + (firstNumber + secondNumber));
+	}
+}
